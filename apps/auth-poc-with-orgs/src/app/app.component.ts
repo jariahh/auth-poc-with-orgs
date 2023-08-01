1a06844d1f515acb56d84679c788bce644a33c4a
ASM\jariah.holsapple <jariah.holsapple@advantagesolutions.net>
2023-08-01 08:54:42 -0400

    Initial commit

---
+++ apps/auth-poc-with-orgs/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'auth-poc-with-orgs-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'auth-poc-with-orgs';
+}
